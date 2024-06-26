1e7f310759ecdfbd13f78841db91808169b69c68
mjlee <amor4330@naver.com>
2024-06-26 17:53:24 +0900

    welcome page

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Scope;
 
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
